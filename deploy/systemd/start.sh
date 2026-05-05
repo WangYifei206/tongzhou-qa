d7813a2920d15e9c44c94a0003256346dde4843a
WangYifei206 <2898056375@qq.com>
2026-05-05 11:56:45 +0800

    prepare repo for public release

--- deploy/systemd/start.sh
+++ deploy/systemd/start.sh
@@ -4,6 +4,6 @@ set -e
 cd /opt/tongzhou
 
 exec /usr/bin/java -jar /opt/tongzhou/ruoyi-admin.jar \
-  --spring.datasource.druid.master.url="jdbc:mysql://127.0.0.1:3306/ry_vue?useUnicode=true&characterEncoding=utf8&zeroDateTimeBehavior=convertToNull&useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=GMT%2B8" \
-  --spring.datasource.druid.master.username=ruoyi \
-  --spring.datasource.druid.master.password="123456."
+  --spring.datasource.druid.master.url="jdbc:mysql://127.0.0.1:3306/your_database?useUnicode=true&characterEncoding=utf8&zeroDateTimeBehavior=convertToNull&useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=GMT%2B8" \
+  --spring.datasource.druid.master.username=your_db_username \
+  --spring.datasource.druid.master.password="your_db_password"
